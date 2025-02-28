0b568a33fdfc19b4d3f2509b622ba5735c8f618a
HotFish <admin@hot.fish>
2025-02-28 13:31:03 -0500

    Get flake and cargo set up

--- flake.nix
+++ flake.nix
@@ -1,24 +1,50 @@
 {
-    description = "Oosikle";
+  description = "Oosikle";
   inputs = {
-      
+
     naersk.url = "github:nix-community/naersk/master";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, utils, naersk }:
-    utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+      rust-overlay,
+      naersk,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
       let
-        pkgs = import nixpkgs { inherit system; };
-        naersk-lib = pkgs.callPackage naersk { };
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rust = pkgs.rust-bin.stable.latest.default;
+        naersk-lib = naersk.lib."${system}".override {
+          cargo = rust;
+          rustc = rust;
+        };
       in
       {
         defaultPackage = naersk-lib.buildPackage ./.;
-        devShell = with pkgs; mkShell {
-          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
-        };
+        devShell =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              eza
+              fd
+              pre-commit
+              rust
+            ];
+            RUST_SRC_PATH = rust;
+
+            shellHook = ''
+              alias ls=eza
+              alias find=fd
+            '';
+          };
       }
     );
 }
